cb34fd7c6e524fcd09b92ba65863df3a47f5e895
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-30 00:16:39 +0700

    Using the Python List sort() method to sort a list of numbers

--- main.py
+++ main.py
@@ -17,3 +17,15 @@ if __name__ == "__main__":
   print(guests)
   guests.sort(reverse=True)
   print(guests)
+
+  # Using the Python List sort() method to sort a list of numbers
+
+  scores = [5, 7, 4, 6, 9, 8]
+  print(scores)
+  scores.sort()
+  print(scores)
+
+  scores = [5, 7, 4, 6, 9, 8]
+  print(scores)
+  scores.sort(reverse = True)
+  print(scores)
